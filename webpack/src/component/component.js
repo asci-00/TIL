78b875395921ca37563350c28d98b03432d18550
count1548 <w_lan123@naver.com>
2021-09-21 21:13:38 +0900

    Dev: Example Source 파일

---
+++ webpack/src/component/component.js
@@ -0,0 +1,1 @@
+export default function component() { console.log('component') }
---
+++ webpack/src/index.js
@@ -0,0 +1,7 @@
+import Component from './component/component'
+import Router from './component/component'
+import './assets/css/common.css'
+export default function mainFunction() {
+    Component()
+    Router()
+}
