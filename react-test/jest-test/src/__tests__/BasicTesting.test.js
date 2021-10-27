93ba05cb24ab421c670e2a6b4f6018b35138ba8b
w_lan123@naver.com <asci-00>
2021-10-27 17:01:01 +0900

    테스트 사항 추가

--- react-test/jest-test/src/__tests__/BasicTesting.test.js
+++ react-test/jest-test/src/__tests__/BasicTesting.test.js
@@ -1,5 +1,11 @@
-import { sum } from '../assets/js/common'
+import { sum, sumList } from '../assets/js/common'
 
-it('1 + 2 = 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+describe('더하기 시나리오', () => {
+    it('sum : 1 + 2 = 3', () => {
+        expect(sum(1, 2)).toBe(3);
+    });
+    it('sum List : 1 ~ 10 = ?', () => {
+        const list = (new Array(10).fill(0)).map((item, idx) => (idx + 1))
+        expect(sumList(list)).toBe(55);
+    });
+})
