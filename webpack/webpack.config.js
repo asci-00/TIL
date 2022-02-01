5e3caa05631c57d2a0bd824412d2fb41e0011f52
NE5054 <w_lan123@naver.com>
2022-02-01 19:03:07 +0900

    feat: (webpack.config.js) entry, output, devtool, rules 적용

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,21 +1,19 @@
-const { resolve } = require('path')
+const { resolve } = require('path');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: resolve(__dirname, 'dist')
+    path: resolve(__dirname, 'dist'),
   },
+  devtool: 'inline-source-map',
   module: {
     rules: [
-      {test : /\.js$/, use: 'babel-loader'},//babel-loader use
+      { test: /\.js$/, use: 'babel-loader' }, //babel-loader use
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      }
-    ]
-  }
-}
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
